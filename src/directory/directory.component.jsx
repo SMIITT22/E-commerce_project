187f973e12bf0cb4fc6c623ba26708a6f3fa817d
SMIITT22 <prosmit2707@gmail.com>
2022-11-15 15:47:07 +0100

    updating the page by making indepenent components

---
+++ src/directory/directory.component.jsx
@@ -0,0 +1,13 @@
+import CategoryItem from '..//components/category-item/category-item.component';
+import './directory.styles.scss'
+const Directory = ({categories}) => {
+    return (
+        <div className="categories-container">
+            {categories.map((category) => (
+                <CategoryItem key={category.id} category={category} />
+            ))}
+        </div>
+    )
+}
+
+export default Directory;
